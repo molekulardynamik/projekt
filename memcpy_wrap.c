d44b54ea3d239f91f4e820ec8e8e385f5749155f
molekulardynamik <molekulardynamik@users.noreply.github.com>
2014-12-11 18:22:48 +0100

    Update memcpy_wrap.c

--- memcpy_wrap.c
+++ memcpy_wrap.c
@@ -1,5 +1,5 @@
 #include <string.h>
-asm (".symver memcpy, memcpy@GLIBC_2.14");
+asm (".symver memcpy, memcpy@GLIBC_2.11.3");
 void *__wrap_memcpy(void *dest, const void *src, size_t n)
 {
 return memcpy(dest, src, n);
